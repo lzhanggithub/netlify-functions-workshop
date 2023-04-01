fa1fa73659138e8c2402b63d9db5ed14f13cf717
liang <leonzhangstudent@gmail.com>
2023-04-01 12:09:09 +0200

    working fetch request

--- lessons-code-complete/use-cases/5-fetching-data/functions/node-fetch/node-fetch.js
+++ lessons-code-complete/use-cases/5-fetching-data/functions/node-fetch/node-fetch.js
@@ -1,25 +1,25 @@
-//const fetch = require('node-fetch')
+const fetch = require("node-fetch");
 
-const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts'
+const API_ENDPOINT =
+  "https://data.mongodb-api.com/app/data-czvsz/endpoint/matchScenario?prolificInput=abcd1234";
 
 exports.handler = async (event, context) => {
-  let response
+  let response;
   try {
-    response = await fetch(API_ENDPOINT)
+    response = await fetch(API_ENDPOINT);
+    response = await response.text();
     // handle response
   } catch (err) {
     return {
       statusCode: err.statusCode || 500,
       body: JSON.stringify({
-        error: err.message
-      })
-    }
+        error: err.message,
+      }),
+    };
   }
 
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      data: response
-    })
-  }
-}
+    body: JSON.stringify(response),
+  };
+};
